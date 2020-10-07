7be7c50523b32411bd7487523b385909756fda04
PierrePocheron <pierre.pocheron@gmail.com>
2020-10-07 15:55:44 +0200

    Exercice 101 OK

--- src/main/java/com/ipiecoles/java/java220/Employe.java
+++ src/main/java/com/ipiecoles/java/java220/Employe.java
@@ -1,7 +1,13 @@
 package com.ipiecoles.java.java220;
+import org.joda.time.LocalDate;
 
 /**
  * Created by pjvilloud on 21/09/17.
  */
 public class Employe {
+    public static  String nom;
+    public static  String prenom;
+    public static  String matricule;
+    public static  LocalDate dateEmbauche;
+    public static  Double salaire;
 }
